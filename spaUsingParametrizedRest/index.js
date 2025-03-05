6ae32b2c44b8ec040bf2f3fddcf990b9b5c1d9d7
Jana Stefanie <s2400789@BC-CWRW4DVTW1-Kannettava-tietokone.local>
2025-03-05 10:06:17 +0200

    add baseUrl variable

--- spaUsingParametrizedRest/index.js
+++ spaUsingParametrizedRest/index.js
@@ -12,22 +12,24 @@ const { getAddressOfOrder } = require('./libraries/addressLibrary'); // no param
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public'))); // to serve the public web pages
 
+const baseRestUrl = 'http://localhost:4003/api/orders';
+
 const homePath = path.join(__dirname, 'menu.html');
 
 app.get('/', (req, res) => res.sendFile(homePath));
 
 app.get('/all', (req, res) =>
-    fetch('http://localhost:4003/api/orders')
+    fetch(baseRestUrl)
         .then(data => data.json())
         .then(result => res.json(result)));
 
 app.get('/report/:id', (req, res) =>
-    fetch(`http://localhost:4003/api/orders/${req.params.id}`)
+    fetch(`${baseRestUrl}/${req.params.id}`)
         .then(data => data.json())
         .then(result => res.json(createFullReport(result))));
 
 app.get('/address/:id', (req, res) =>
-    fetch(`http://localhost:4003/api/orders/${req.params.id}`)
+    fetch(`${baseRestUrl}/${req.params.id}`)
         .then(data => data.json())
         .then(result => res.json(getAddressOfOrder(result[0])))); // taking the first item of the array
 
